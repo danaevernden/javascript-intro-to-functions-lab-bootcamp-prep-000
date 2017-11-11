64b7ff0baeb54fe1a4c7c502d8ea3e39708b76f2
Dana Evernden <devernde@gmail.com>
2017-11-11 19:54:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string = string.toLowerCase())
-  "I can't hear you!"
+  if(string = string.toLowerCase()) 
+{  "I can't hear you!"
   else if(string = string.toUpperCase())
   "YES INDEED!"
   else if(string = "I love you, Grandma.")
