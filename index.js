6d012267a2dd62644be2fda215a00b35de6536f8
Dana Evernden <devernde@gmail.com>
2017-11-11 19:58:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function sayHiToGrandma(string) {
    else if(string === string.toUpperCase())
 { return "YES INDEED!" ;}
   else if(string === "I love you, Grandma.")
- { return lI love you, too." ;}
+ { return "I love you, too." ;}
 }
 
